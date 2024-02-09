ca0306557a3c059e8622e3bba74801fdbfa109c1
devR2001 <144663791+devR2001@users.noreply.github.com>
2024-02-09 12:28:49 +0100

    First Components & Props

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <ul>
-    <li></li>
+    <LearningResource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    />
   </ul>
 </template>
 
 <script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
 export default {
+  components: {
+    LearningResource,
+  },
   data() {
     return {
       storedResources: [
